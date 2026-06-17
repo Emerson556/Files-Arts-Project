cf7edc32c3509372f4ab7aae5809f6e004b931ee
Emerson556 <Emerson.coleman56746@gmail.com>
2026-06-17 14:05:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 console.log("Website Loaded");
+const images = [
+"images/2015-1.jpg",
+"images/2015-2.jpg",
+"images/2015-3.jpg",
+"images/2015-4.jpg",
+"images/2015-5.jpg"
+];
+
+let currentImage = 0;
+
+function nextImage() {
+currentImage++;
+
+
+if (currentImage >= images.length) {
+    currentImage = 0;
+}
+
+document.getElementById("gallery-image").src =
+    images[currentImage];
+
+
+}
+
+function previousImage() {
+currentImage--;
+
+
+if (currentImage < 0) {
+    currentImage = images.length - 1;
+}
+
+document.getElementById("gallery-image").src =
+    images[currentImage];
+
+
+}
